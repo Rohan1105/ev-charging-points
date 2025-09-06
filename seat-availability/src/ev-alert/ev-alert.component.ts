baf222bb2421812535f96fbe9623f7bde895c786
Rohan Pal <rohanpal.kgp@gmail.com>
2025-09-07 02:13:28 +0530

    Made a form of country and cities along with info req and made it more dynamic

--- seat-availability/src/ev-alert/ev-alert.component.ts
+++ seat-availability/src/ev-alert/ev-alert.component.ts
@@ -1,7 +1,18 @@
-import { Component } from '@angular/core';
+import { Component,OnInit } from '@angular/core';
 import { NgIf, NgFor } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { ApiService } from '../services/api.service';
+import demographics from '../../configs/cities.json';
+
+interface CityData {
+  country: string;
+  countryCode: string;
+  cities: {
+    name: string;
+    latitude: number;
+    longitude: number;
+  }[];
+}
 
 @Component({
   selector: 'app-ev-alert',
@@ -10,17 +21,58 @@ import { ApiService } from '../services/api.service';
   templateUrl: './ev-alert.component.html',
   styleUrls: ['./ev-alert.component.scss']
 })
-export class EvAlertComponent {
+export class EvAlertComponent implements OnInit{
 
   constructor(public apiService : ApiService){}
+  list_of_countries:String[] =[];
+  country:CityData[] = [];
+  list_of_cities:String[] = [];
   city: string = '';
   country_code : string = '';
+  c_code : string = '';
   latitude : number = 0;
   longitude: number = 0;
   stations: any[] = [];
+  latitudeDisabled = true;
+  longitudeDisabled = true;
+
+  ngOnInit(): void {
+    demographics.forEach((metro_city: CityData)=>{
+      this.list_of_countries.push(metro_city.country);
+    })
+  }
+
+  countryDetails(){
+    this.latitude = 0;
+    this.longitude = 0;
+    this.list_of_cities = [];
+    this.country = demographics.filter((res: CityData) => {
+      return res.country == this.country_code;
+    })
+    for(var i=0;i<demographics.length;i++){
+      if(demographics[i]['country'] == this.country_code){
+        this.c_code = demographics[i]['countryCode'];
+      }
+    }
+    var cities = this.country[0]['cities'];
+    for(var i=0;i<cities.length;i++){
+      this.list_of_cities.push(cities[i]['name']);
+    }
+  }
+
+  cityDetails(){
+    console.log(this.city);
+    var cities = this.country[0]['cities'];
+    for(var i=0;i<cities.length;i++){
+      if(cities[i]['name'] == this.city){
+        this.latitude = cities[i]['latitude'];
+        this.longitude = cities[i]['longitude'];
+      }
+    }
+  }
 
   onFetchStations() {
-    this.apiService.fetchSeatAvailabilty(this.country_code,this.latitude,this.longitude).subscribe((stationData =>{
+    this.apiService.fetchSeatAvailabilty(this.c_code,this.latitude,this.longitude).subscribe((stationData =>{
       try {
         console.log(stationData);
         stationData.forEach((element : any) => { // add interface
